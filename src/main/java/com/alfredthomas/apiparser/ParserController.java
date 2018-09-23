6512d77ef2ba19602c50a2fb81dfc6e24ac45396
AJ-PC\AJ <alfredthomasiii@gmail.com>
2018-09-22 19:20:31 -0700

    tweaking result to be optionally named via "name" parameter

--- src/main/java/com/alfredthomas/apiparser/ParserController.java
+++ src/main/java/com/alfredthomas/apiparser/ParserController.java
@@ -3,6 +3,7 @@ package com.alfredthomas.apiparser;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.core.io.ClassPathResource;
@@ -22,17 +23,19 @@ public class ParserController {
     private final String defaultOutPath = "zips/";
 
     @RequestMapping(value = "/parse", method = RequestMethod.GET, produces = "application/zip")
-    public ResponseEntity<InputStreamResource> parseURL(@RequestParam(value="url")String url)
+    public ResponseEntity<InputStreamResource> parseURL(@RequestParam(value="url")String url,@RequestParam(value="name",defaultValue = "")String name)
     {
         String filename = Long.toString(counter.incrementAndGet());
+        if(name.isEmpty())
+            name="result";
         try{
             ClassParser.parseAllClasses(Util.getAllClassDocument(url),defaultOutPath+filename);
-//            ClassPathResource zipFile = new ClassPathResource(defaultOutPath + filename+".zip");
             File zipFile = new File(defaultOutPath + filename+".zip");
 
             return ResponseEntity.ok()
                     .contentLength(zipFile.length())
                     .contentType(MediaType.parseMediaType("application/zip"))
+                    .header("Content-disposition", "attachment; filename="+ name+".zip")
                     .body(new CleanupInputStreamResource(zipFile));
         }
         catch (Exception e)
@@ -42,8 +45,8 @@ public class ParserController {
 
         }
     }
-    public class CleanupInputStreamResource extends InputStreamResource {
-        public CleanupInputStreamResource(File file) throws FileNotFoundException {
+    private class CleanupInputStreamResource extends InputStreamResource {
+        private CleanupInputStreamResource(File file) throws FileNotFoundException {
             super(new FileInputStream(file) {
                 @Override
                 public void close() throws IOException {
